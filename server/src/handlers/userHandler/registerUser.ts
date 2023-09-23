5fba23b5f695f09a857a12f0f9d3c10c86acd1f5
EzequielMenendez <ezemenendez2003@gmail.com>
2023-09-23 11:16:01 -0300

    feat(registerUser):add hash password

--- server/src/handlers/userHandler/registerUser.ts
+++ server/src/handlers/userHandler/registerUser.ts
@@ -1,9 +1,27 @@
 import User from "../../models/userModel"
 import { UserData } from "../../types"
+import bcrypt from 'bcryptjs'
 
 const registerUser = async(body: UserData)=>{
-    const newUser = new User(body)
-    return await newUser.save().then((data)=>data).catch((err)=>{throw new Error(err)})
+    try {
+        const { name, email, password } = body
+
+        const hash:string = await bcrypt.hash(password, 10)
+    
+        const newUser = new User({
+            name,
+            email,
+            password: hash
+        })
+    
+        const userCreated = await newUser.save()
+        return {
+            name: userCreated.name,
+            email: userCreated.email
+        }
+    } catch (error:any) {
+        throw new Error(error.message);
+    }
 }
 
 export default registerUser
