bdb372305e1210c3c961249ffdf5ee16202fb2f4
EzequielMenendez <ezemenendez2003@gmail.com>
2023-09-23 18:55:40 -0300

    feat(loginUser): add login handler

--- server/src/handlers/userHandler/loginUser.ts
+++ server/src/handlers/userHandler/loginUser.ts
@@ -1,8 +1,32 @@
 import userModel from "../../models/userModel"
+import { Response, Request } from 'express';
+import bcrypt from 'bcryptjs'
+import createToken from "../../libs/jwt";
 
-const loginUser = async(id:string)=>{
-    const user = await userModel.findById(id)
-    return user
+const loginUser = async(req: Request, res: Response)=>{
+    const { email, password } = req.body
+    try {
+        const user = await userModel.findOne({email})
+
+        if(!user){
+            res.status(404).json({error: 'User not found'})
+            return
+        }
+        const hash = await bcrypt.compare(password, user.password)
+
+        if(!hash){
+            res.status(400).json({ error: 'Incorrect password'})
+            return
+        }
+
+        const token = await createToken({ id: user._id})
+
+        res.cookie("token", token)
+        res.status(200).json({message: 'User logged'})
+
+    } catch (error: any) {
+        res.status(500).json({error: error.message})
+    }
 }
 
 export default loginUser
--- server/src/routes/userRouter.ts
+++ server/src/routes/userRouter.ts
@@ -4,12 +4,10 @@ import loginUser from '../handlers/userHandler/loginUser'
 
 const userRouter = express.Router()
 
-userRouter.post('/login', async(req, res)=>{
-    const { id } = req.body
-    const user = await loginUser(id)
-    res.status(200).json(user)
-})
+userRouter.post('/login', loginUser)
 
 userRouter.post('/register', registerUser)
+
+userRouter.post('/logout')
 
 export default userRouter
