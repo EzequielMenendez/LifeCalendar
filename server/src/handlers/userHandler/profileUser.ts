848e8144fce391f491a86e6dc1494464753ee5d0
EzequielMenendez <ezemenendez2003@gmail.com>
2023-09-24 17:36:08 -0300

    feat(profileUser): add validate Token

--- server/src/handlers/userHandler/profileUser.ts
+++ server/src/handlers/userHandler/profileUser.ts
@@ -1,7 +1,17 @@
 import { Response, Request } from 'express';
+import User from "../../models/userModel"
 
-const profileUser = (req: Request, res: Response) =>{
-    res.send(req.user)
+const profileUser = async(req: Request, res: Response) =>{
+    const user = await User.findById(req.user?.id)
+    if(!user){
+        res.status(404).json({message: 'User not found'})
+        return
+    }
+
+    return res.status(200).json({
+        id: user._id,
+        name: user.name,
+        email: user.email
+    })
 }
-
 export default profileUser
