d1d8872e173b7529bf28799505bf0f934c8990c3
EzequielMenendez <ezemenendez2003@gmail.com>
2023-09-25 19:06:18 -0300

    feat(App):add react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 function App() {
   return (
     <div>
-      <p className="text-4xl font-bold">Hola</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Hola</h1>}/>
+          <Route path="/login" element={<h1>Hola</h1>}/>
+          <Route path="/register" element={<h1>Hola</h1>}/>
+          <Route path="/profile" element={<h1>Hola</h1>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
