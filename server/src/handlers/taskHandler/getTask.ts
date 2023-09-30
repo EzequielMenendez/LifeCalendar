b23814757a66977118bb3bcca57d72b462e26f34
EzequielMenendez <ezemenendez2003@gmail.com>
2023-09-30 18:56:11 -0300

    feat(getTask):add controller getTask

--- server/src/handlers/taskHandler/getTask.ts
+++ server/src/handlers/taskHandler/getTask.ts
@@ -3,23 +3,27 @@ import { Response, Request } from 'express';
 import { TaskData } from '../../types';
 
 const getTask = async(_req:Request, res:Response)=>{
-    const task:Array<TaskData> | null = await taskModel.find()
+    try {
+        const task:Array<TaskData> | null = await taskModel.find()
 
-    if(!task){
-        res.status(404).json({error: 'taskNotFound'})
-        return
-    }
-
-    const filterTask = task.map((t:TaskData)=>{
-        return {
-            id: t._id,
-            name: t.name,
-            startDate: t.startDate,
-            endDate: t.endDate
+        if(!task){
+            res.status(404).json({error: 'taskNotFound'})
+            return
         }
-    })
-
-    res.status(200).json(filterTask)
+    
+        const filterTask = task.map((t:TaskData)=>{
+            return {
+                id: t._id,
+                name: t.name,
+                startDate: t.startDate,
+                endDate: t.endDate
+            }
+        })
+    
+        res.status(200).json(filterTask)
+    } catch (error: any) {
+        res.status(500).json({error: error.message})
+    }
 }
 
 export default getTask
