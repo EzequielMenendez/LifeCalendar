8b9948a3907db6b78b2179fcf0aeec31447d2c4a
EzequielMenendez <ezemenendez2003@gmail.com>
2023-10-02 17:50:35 -0300

    feat(taskForm): add react-datepicker

--- client/src/components/TaskForm/TaskForm.tsx
+++ client/src/components/TaskForm/TaskForm.tsx
@@ -1,11 +1,40 @@
 import { useForm } from "react-hook-form"
+import { Task } from "../../types"
+import { postTaskRequest } from "../../api/auth"
+import DatePicker from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css"
+import { useState } from 'react'
 
 function TaskForm(){
     const {register, handleSubmit, formState:{errors}} = useForm()
+    const [ startDate, setStartDate ] = useState(new Date())
+    const [ endDate, setEndDate ] = useState(new Date())
+    const [ minDate, _setMinDate ] = useState(new Date())
+    const [ minTime, _setMinTime] = useState(new Date(0, 0, 0, 0, 0))
+    console.log(minTime)
+
+    const onSubmit = (async(values:Task)=>{
+        try {
+            await postTaskRequest(values)
+            
+        } catch (error) {
+            
+        }
+    })
+
+    const onChangeStart = (date:Date) => {
+        setStartDate(date)
+        console.log(date)
+    }
+
+    const onChangeEnd = (date:Date) => {
+        setEndDate(date)
+        console.log(date)
+    }
 
     return(
         <div>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <input
                 type="title"
                 id="title"
@@ -16,6 +45,25 @@ function TaskForm(){
                 className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
                 />
                 {errors.title && <p className='text-red-500'>Title is required</p>}
+                <DatePicker 
+                selected={startDate}
+                onChange={(date:Date) => onChangeStart(date)}
+                withPortal
+                showTimeSelect
+                minDate={minDate}
+                dateFormat="Pp"
+                className="text-black"
+                />
+
+                <DatePicker 
+                selected={endDate}
+                onChange={(date:Date) => onChangeEnd(date)}
+                withPortal
+                showTimeSelect
+                minDate={startDate}
+                dateFormat="Pp"
+                className="text-black"
+                />
                 <button type='submit'>Create Task</button>
             </form>
         </div>
