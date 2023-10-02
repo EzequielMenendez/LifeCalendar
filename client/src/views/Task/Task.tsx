7f9b356700e7b2ad6011dc7c60e2211d9c7ddcf6
EzequielMenendez <ezemenendez2003@gmail.com>
2023-10-02 16:17:39 -0300

    feat(Task): add popup createTask

--- client/src/views/Task/Task.tsx
+++ client/src/views/Task/Task.tsx
@@ -1,10 +1,31 @@
 import TaskForm from "../../components/TaskForm/TaskForm"
+import { useState } from 'react'
 
 function Task(){
+    const [ showAlert, setShowAlert ] = useState(false)
+
+    const handleCloseAlert = () => {
+        setShowAlert(false)
+    }
+
+    const handleOpenAlert = () => {
+        setShowAlert(true)
+    }
+
     return(
         <div>
+            {showAlert && (
+                <div className="pop-ups">
+                    <div className="flex h-[calc(100vh-100px)] items-center justify-center">
+                        <div className="bg-zinc-800 max-w-md p-10 rounded-md">
+                            <TaskForm/>
+                            <button onClick={()=> handleCloseAlert()}>X</button>
+                        </div>
+                    </div>
+                </div>
+            )}
             <h1>Task</h1>
-            <TaskForm></TaskForm>
+            <button onClick={()=> handleOpenAlert()}>create Task</button>
         </div>
     )
 }
