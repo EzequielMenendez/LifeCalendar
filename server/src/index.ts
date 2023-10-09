d5b0e51fa1d327934bb253b8037e014e3e9b9a8b
EzequielMenendez <ezemenendez2003@gmail.com>
2023-10-09 12:45:18 -0300

    deploy

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,7 +16,7 @@ if(process.env.DB_URL){
 
 const server = express()
 server.use(cors({
-    origin: '*',
+    origin: 'https://life-calendar-5rji.vercel.app/',
     credentials: true
 }))
 server.use(express.json())
