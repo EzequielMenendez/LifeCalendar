29b0a10483d1ec802a4fd8897bb61038dd4ac65f
EzequielMenendez <ezemenendez2003@gmail.com>
2023-10-11 11:33:47 -0300

    feat(all):delete deploy

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -1,34 +1,34 @@
 import axios from "./axios";
 import { RegisterUser, LoginUser, Task } from "../types";
 
-export const registerRequest = async(user: RegisterUser) => {
+export const registerRequest = (user: RegisterUser) => {
     return axios.post(`/user/register`, user)
 }
 
-export const loginRequest = async(user:LoginUser) => {
+export const loginRequest = (user:LoginUser) => {
     return axios.post(`/user/login`, user)
 }
 
-export const postTaskRequest = async(task: Task) => {
+export const postTaskRequest = (task: Task) => {
     return axios.post('/task', task)
 }
 
-export const getAllTaskRequest = async() => {
+export const getAllTaskRequest = () => {
     return axios.get('/task')
 }
 
-export const getTaskRequest = async(id: string) => {
+export const getTaskRequest = (id: string) => {
     return axios.get(`/task/${id}`)
 }
 
-export const putTaskRequest = async(id: string, task: Task) => {
+export const putTaskRequest = (id: string, task: Task) => {
     return axios.put(`/task/${id}`, task)
 }
 
-export const deleteTaskRequest = async(id: string) => {
+export const deleteTaskRequest = (id: string) => {
     return axios.delete(`/task/${id}`)
 }
 
-export const verifyTokenRequest = async() => axios.get('/user/verify')
+export const verifyTokenRequest = () => axios.get('/user/verify')
 
-export const logoutRequest = async() => axios.post('/user/logout')
+export const logoutRequest = () => axios.post('/user/logout')
